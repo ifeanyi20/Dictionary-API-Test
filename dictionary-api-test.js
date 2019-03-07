bb87be7aa4f1bda4d6de301f0fc0c4aa7b5a5a2f
ifeanyi20 <48307665+ifeanyi20@users.noreply.github.com>
2019-03-07 10:05:33 -0600

    Update dictionary-api-test.js

--- dictionary-api-test.js
+++ dictionary-api-test.js
@@ -1,6 +1,8 @@
 /* Test Cases for testing Dictionary API using Java Script as languange*/
 /* These tests have been created in Postman and exported the Java Script code from Postman */
 
+
+API_KEY= process.env['DICT_API_KEY']
 /* Test for creating Dictionary */
 var settings = {
   "async": true,
@@ -9,7 +11,7 @@ var settings = {
   "method": "POST",
   "headers": {
     "Content-Type": "application/json",
-    "Authorization": "Basic API_KEY",
+    "Authorization": "Basic" + API_KEY,
     "cache-control": "no-cache",
     "Postman-Token": "203f390e-fdd5-4f35-8b27-a587f6cfbf95"
   },
@@ -29,7 +31,7 @@ var settings = {
   "method": "DELETE",
   "headers": {
     "Content-Type": "application/json",
-    "Authorization": "Basic API_KEY",
+    "Authorization": "Basic" + API_KEY,
     "cache-control": "no-cache",
     "Postman-Token": "59de4c57-0cbd-416d-9475-0c813915e126"
   },
@@ -49,7 +51,7 @@ var settings = {
   "method": "POST",
   "headers": {
     "Content-Type": "application/json",
-    "Authorization": "Basic API_KEY",
+    "Authorization": "Basic" + API_KEY,
     "cache-control": "no-cache",
     "Postman-Token": "5ef9ed93-dff3-499c-b103-cb3627e30cd8"
   },
@@ -69,7 +71,7 @@ var settings = {
   "method": "GET",
   "headers": {
     "Content-Type": "application/json",
-    "Authorization": "Basic API_KEY",
+    "Authorization": "Basic" + API_KEY,
     "cache-control": "no-cache",
     "Postman-Token": "4ee618b1-6fbf-4d91-81e1-3f2f4cdb0d39"
   },
@@ -89,7 +91,7 @@ var settings = {
   "method": "GET",
   "headers": {
     "Content-Type": "application/json",
-    "Authorization": "Basic API_KEY",
+    "Authorization": "Basic" + API_KEY,
     "cache-control": "no-cache",
     "Postman-Token": "ec98c65b-f14e-4168-96db-d4577d8b6742"
   },
@@ -110,7 +112,7 @@ var settings = {
   "method": "DELETE",
   "headers": {
     "Content-Type": "application/json",
-    "Authorization": "Basic API_KEY",
+    "Authorization": "Basic" + API_KEY,
     "cache-control": "no-cache",
     "Postman-Token": "0df4a887-ac57-4ff1-9f7b-08869a5bf37b"
   },
